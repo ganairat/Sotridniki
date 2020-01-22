6f47907b14e127d9076b231f9d15e4e518eaca92
Айрат Ганеев <ganeevairatt@live.com>
2020-01-22 13:22:24 +0300

    add

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sotrudniki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = "../../input.txt";
+            string outputPath = "../../output.txt";
+            int n = 0;
+            Person boss = new Person{ Id = 0, BossId=0, Name=""};
+            List<Person> people = new List<Person>();
+            using (StreamReader sr = new StreamReader(inputPath, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var t = line.Split('|');
+                    Person p = new Person { Id = int.Parse(t[0]), BossId = int.Parse(t[1]), Name = t[2] };
+                    people.Add(p);
+                    n = Math.Max(n, p.Id);
+                    if (p.BossId == 0) boss = p;
+                }
+            }
+            n++;
+            int[,] g = new int[n, n];
+            bool[] visited = new bool[n];
+            Person[] persons = new Person[n];
+            foreach(var p in people)
+            {
+                persons[p.Id] = p;
+                g[p.BossId, p.Id] = 1;
+
+            }            
+            dfs(boss.Id, 0);
+            void dfs(int v, int l)
+            {
+
+                using (StreamWriter sw = new StreamWriter(outputPath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(new string('-', l) + persons[v].Name);
+                }
+                visited[v] = true;
+                for (int i = 0; i < n; i++)
+                {
+                    if (g[v, i] == 1 && !visited[i])
+                    {
+                        dfs(i, l + 1);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Sotrudniki")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sotrudniki")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2b1325dd-27ff-45ac-b716-359bba52032d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
